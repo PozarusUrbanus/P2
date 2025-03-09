07fef20739d3a96118f6c2be34ec07ca990be9bb
Urbanus <urban.pozar640@gmail.com>
2025-03-09 13:47:45 +0100

    update

--- src/Kvizi/Kviz1/kviz1_13.java
+++ src/Kvizi/Kviz1/kviz1_13.java
@@ -1,32 +1,21 @@
 package Kvizi.Kviz1;
 
 public class kviz1_13 {
-    public static boolean jePrastevilo(int n) {
-        for (int i = 2; i < n; i++) {
-            if (n % i == 0) {
-                return false;
+    public static void pitagoroviTrojcki (int x) {
+
+        for (int a = 1; a <= x; a++) {
+            for (int b = a; b <= x; b++) {
+                for (int c = b; c <= x; c++) {
+                    if (a*a + b*b == c*c) {
+                        System.out.printf(a + " " + b + " " + c + "\n");
+                    }
+                }
             }
         }
-        return n > 0;
     }
-    public static int vsotaPrvih(int n) {
-        int vsota = 0;
-        int st = 2;
-        int st_Prast = 0;
-
-        while (st_Prast < n) {
-            if (jePrastevilo(st)) {
-                vsota += st;
-                st_Prast++;
-            }
-            st++;
-        }
-        return vsota;
-    }
-
 
     public static void main(String[] args) {
-        System.out.println(vsotaPrvih(10));
+        pitagoroviTrojcki(25);
     }
 
 }
